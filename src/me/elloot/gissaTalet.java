f7f9a7d3df6361000170843292556dfaef2f5fbc
elloot <elloot@github.com>
2019-10-17 10:48:05 +0200

    Initial commit

---
+++ src/me/elloot/gissaTalet.java
@@ -0,0 +1,13 @@
+package me.elloot;
+
+/*
+    A simple program that lets you guess a number
+    2019-10-17
+    Author: Elliot Duchek  
+*/
+
+public class gissaTalet {
+    public static void main(String[] args) {
+        
+    }
+}
